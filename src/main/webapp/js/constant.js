593b39c51f2444bc02a7043f21bbca8646fbf6f8
yuanyijie <821623879@qq.com>
2017-05-07 23:54:57 +0800

    login page

---
+++ src/main/webapp/js/constant.js
@@ -0,0 +1,12 @@
+/**
+ * 常量js
+ */
+var APPID = "/feed";
+
+String.prototype.format = function() {
+	if (arguments.length == 0)
+		return this;
+	for (var s = this, i = 0; i < arguments.length; i++)
+		s = s.replace(new RegExp("\\{" + i + "\\}", "g"), arguments[i]);
+	return s;
+};
